729b5c8f87ee82e51ae43e63bea6e2603cf95220
emiliya-san <61347354+emiliya-san@users.noreply.github.com>
2020-06-05 21:02:35 +0100

    Adding movable logo header

--- scroll.js
+++ scroll.js
@@ -1,17 +1,24 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+const body = document.body;
 
-// Get the header
-var header = document.getElementById("myHeader");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll == 0) {
+    body.classList.remove(scrollUp);
+    return;
   }
-}
+
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
